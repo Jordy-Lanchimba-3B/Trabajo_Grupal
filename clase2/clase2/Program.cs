1d4346f060521fa7f8c1f8bac1c85c33dbe13226
Jordy-Lanchimba-3B <56418626+Jordy-Lanchimba-3B@users.noreply.github.com>
2019-10-21 22:05:35 -0500

    Add files via upload

---
+++ clase2/clase2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using clase2.clases;
+namespace clase2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nota nota = new Nota();
+            try
+            {
+                nota.Nombre = "Lauren Michelle Jauregui";
+                nota.Actividad1p = 7;
+                nota.Practica1p = 5;
+                nota.Tarea1p = 2;
+                nota.EvaluacionFinald1p = 4;
+
+                nota.Actividad2p = 10;
+                nota.Practica2p = 10;
+                nota.Tarea2p = 10;
+                nota.EvaluacionFinald2p = 5;
+
+                nota.Recuperacion = 10;
+
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine("Nombre :"                +nota.Nombre);
+
+            Console.WriteLine(      "*****PRIMER PARCIAL*****"     );
+
+            Console.WriteLine("ACT 1P :"           +nota.Actividad1p);
+            Console.WriteLine("PRAC 1P :"          + nota.Practica1p);
+            Console.WriteLine("TAREA 1P :"            + nota.Tarea1p);
+            Console.WriteLine("EF 1P :" +    nota.EvaluacionFinald1p);
+            Console.WriteLine("PRIMER PARCIAL :"   + nota.Practica1p);
+
+            Console.WriteLine(      "*****SEGUNDO PARCIAL*****"     );
+
+            Console.WriteLine("ACT 2P :"          + nota.Actividad2p);
+            Console.WriteLine("PARC 2P :"          + nota.Practica2p);
+            Console.WriteLine("TAREA 2P :"            + nota.Tarea2p);
+            Console.WriteLine("EF 1P :"    + nota.EvaluacionFinald1p);
+            Console.WriteLine("SEGUNDO PARCIAL:"+nota.SegundoParcial);
+
+            Console.WriteLine("SUMA :"                   + nota.Suma);
+            Console.WriteLine("RECUPERACION :"   + nota.Recuperacion);
+            Console.WriteLine("FINAL :"                 + nota.Final);
+
+            Console.WriteLine( "Estado : "             + nota.Estado);
+
+            Console.WriteLine(      "*****FELICIDADES*****"         );
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
---
+++ clase2/clase2/clases/Nota.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clase2.clases
+{
+     public class Nota
+
+     {
+        private String nombre;
+
+        public String Nombre
+        {
+            get { return nombre; }
+            set {
+                if (value.Split(' ').Length < 3)
+                {
+                    throw new Exception("Nombre incompletos  ");
+                }
+                nombre = value; }
+        }
+
+        private float actividad1p;
+
+        public float Actividad1p
+        {
+            get { return actividad1p; }
+            set {
+                if (value <0 && value >10)
+                {
+                    throw new Exception("Ingrese un numero mayor o igual 1 o menor e igual 10  ");
+                }
+                actividad1p = value; }
+
+        }
+        private float practica1p;
+
+        public float Practica1p
+        {
+            get { return practica1p; }
+            set {
+                if (value < 0 && value >10)
+                {
+                    throw new Exception("Ingrese un numero mayor o igual 1 o menor e igual 10  ");
+                }
+                practica1p = value; }
+        }
+
+        private float tarea1p;
+
+        public float Tarea1p
+        {
+            get { return tarea1p; }
+            set {
+                if (value < 0 && value > 10)
+                {
+                    throw new Exception("Ingrese un numero mayor o igual 1 o menor e igual 10  ");
+                }
+                tarea1p = value; }
+        }
+        private float evaluacionfinal1p;
+
+        public float EvaluacionFinald1p
+        {
+            get { return evaluacionfinal1p; }
+            set {
+                if (value < 0 && value > 10)
+                {
+                    throw new Exception("Ingrese un numero mayor o igual 1 o menor e igual 10  ");
+                }
+                evaluacionfinal1p = value; }
+        }
+        
+
+        public float PrimerParcial
+        {
+            get { return Actividad1p*0.25f +Practica1p*0.25f+Tarea1p*0.20f+evaluacionfinal1p*0.30f; }
+            
+        }
+        private float actividad2p;
+
+        public float Actividad2p
+        {
+            get { return actividad2p; }
+            set {
+                if (value < 0 && value > 10)
+                {
+                    throw new Exception("Ingrese un numero mayor o igual 1 o menor e igual 10  ");
+                }
+                actividad2p = value; }
+
+        }
+        private float practica2p;
+
+        public float Practica2p
+        {
+            get { return practica2p; }
+            set {
+                if (value < 0 && value > 10)
+                {
+                    throw new Exception("Ingrese un numero mayor o igual 1 o menor e igual 10  ");
+                }
+                practica2p = value; }
+        }
+
+        private float tarea2p;
+
+        public float Tarea2p
+        {
+            get { return tarea2p; }
+            set {
+                if (value < 0 && value > 10)
+                {
+                    throw new Exception("Ingrese un numero mayor o igual 1 o menor e igual 10  ");
+                }
+                tarea2p = value; }
+        }
+        private float evaluacionfinal2p;
+
+        public float EvaluacionFinald2p
+        {
+            get { return evaluacionfinal2p; }
+            set {
+                if (value < 0 && value > 10)
+                {
+                    throw new Exception("Ingrese un numero mayor o igual 1 o menor e igual 10  ");
+                }
+                evaluacionfinal2p = value; }
+        }
+
+        public float SegundoParcial
+        {
+            get { return Actividad2p*0.25f +Practica2p*0.25f+Tarea2p*0.20f+EvaluacionFinald2p*0.30f; }
+            
+        }
+        public float Suma
+        {
+            get { return PrimerParcial+SegundoParcial; }
+            
+        }
+        private float recuperacion;
+
+        public float Recuperacion
+        {
+            get { return recuperacion; }
+            set { recuperacion = value; }
+        }
+        public float Suma2
+        {
+            get { return Recuperacion+Suma; } 
+        }
+
+
+        public float Final
+        {
+            get { return Suma2/2; }
+            
+        }
+      
+
+        public String Estado
+        {
+            
+            get
+            {
+                if (Suma >= 14 || Suma2 >= 14)
+                {
+                    return " Aprobado ";
+                }
+                else
+                {
+                    if (Suma < 14)
+                    {
+                        return " Recuperacion ";
+                    }
+                    else
+                    {
+                        return "Aprobado";
+                    }
+                }
+
+
+               
+
+                    
+
+             
+
+                
+            }
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
